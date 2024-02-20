65c6c37d6ecfb805545d69b6fbaa072bb927484f
Miski Yusuf <89570203+Miskiyu@users.noreply.github.com>
2024-02-20 16:42:36 +0100

    Fikset på index siden

--- mnregnskap.client/src/pages/Hjem.jsx
+++ mnregnskap.client/src/pages/Hjem.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import './Hjem.css';
 function Homepage() {
-  return (
-    <div>
-      <h1>Homepage is here</h1>
-    </div>
-  );
+    return (
+        <div className="bilde-seksjon">
+            <div className="bilde-seksjon-img">
+                <img src="/public/index-bildet.jpg" alt="" className="img-style" />
+                <div className="tekst-i-midten">
+                    <h1>M&N</h1>
+                    <h1>REGNSKAP</h1>
+                    <h4>Vi tar vare p&aring; regnskapet!</h4>
+                    <div className="bildet-seksjon-knapp">
+                        <button className="mer-info-knapp">MER INFO</button>
+
+                    </div>
+                </div>
+       
+            </div>
+        </div>
+    );
 }
 
 export default Homepage;
