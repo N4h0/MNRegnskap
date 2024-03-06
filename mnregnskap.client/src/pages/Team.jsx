29bb3fb41a61f73ec0201028710307321ad27aa4
nishat2001 <s358975@oslomet.no>
2024-03-06 19:43:38 +0100

    Fix: Team + om oss side

--- mnregnskap.client/src/pages/Team.jsx
+++ mnregnskap.client/src/pages/Team.jsx
@@ -3,23 +3,68 @@ import './Team.css';
 
 
 function Team() {
+
     const Bildet = () => (
 
         <div className="bilde-seksjon-img">
             <img src="/index-bildet.jpg" alt="" className="img-style" />
             <div className="tekst-i-midten">
-                <h1>Meet the team</h1>
-                <h1>Skal endre bildet :P</h1>
+                <h1>M&N</h1>
+                <h1>REGNSKAP</h1>
+                <h4>Vi tar vare p&aring; regnskapet!</h4>
+                <div className="bildet-seksjon-knapp">
+                    <button className="mer-info-knapp">MER INFO</button>
+                </div>
             </div>
         </div>
 
     );
+
     let message = "Trenger inspirasjon til hva jeg skal skrive her"
     return (
         <>
-            <div className="bilde-seksjon">
-                <Bildet />
+            <div className='aboutIs_box'>
+                <div className='aboutUs_box_hero'>
+                    <div className='aboutUs_box_hero_left'>
+                        <h1>About Us </h1>
+                        <p>
+                            M&N Accounting is a personal accounting firm that communicates
+                            well with its clients, knows their financial history and condition,
+                            and provides services that optimize the client’s financial situation.
+                            Our goal is to be an important contributor to our clients as an accountant and advisor.
+                            We have a broad network of contacts in the field of auditing and legal services.
+                        </p>
+                    </div>
+                    <div className='aboutUs_box_hero_right'>
+                        <div className="bilde-seksjon">
+                            <Bildet />
+                        </div>
+     
+                    </div>
+                </div>
+
+      
+                <div className='aboutUs_box_hero'>
+                    <div className='aboutUs_box_hero_left'>
+                        <div className="bilde-seksjon">
+                            <Bildet />
+                        </div>
+
+                    </div>
+                        <div className='aboutUs_box_hero_right'>
+                            <h1>Historie.</h1>
+                            <p>
+                            M&N Accounting is located centrally on Frogner.
+                            We have extensive experience in accounting in most industries.
+                            We provide accounting services for customers in Oslo and the rest of Norway.
+                            We have employees with a high education and vast experience.
+                            </p>
+                        </div>
+                    </div>
+
             </div>
+
+
             <section className="section-white">
 
                 <div className="container">
