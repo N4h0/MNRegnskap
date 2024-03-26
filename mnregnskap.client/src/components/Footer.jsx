860df4c779c7394917af3c5205e294611d3b064c
Johan Tryti <s362059@oslomet.no>
2024-03-26 14:19:56 +0100

    Footer.
    Endra farge til kvit, fjerna ca. 126 unødvendige divs og klasser, la til nytt bilde, endra litt skrift og styling.

--- mnregnskap.client/src/components/Footer.jsx
+++ mnregnskap.client/src/components/Footer.jsx
@@ -19,16 +19,16 @@ const Footer = () => {
 };
 
 const Godkjenn = () => (
-    <div>
-        <div id="media_image-5">
-            <h1>Godkjenninger</h1>
-            <img width="290" height="150" src="/medlem.png" alt="" />
+    <div className="kontaktinfo">
+        <h1>Godkjenninger</h1>
+        <div>
+            <img width="290" height="150" src="/footerlogo2.png" alt="" />
         </div>
     </div>
 );
 
 const ContactInfo = () => (
-    <div>
+    <div className = "kontaktinfo">
         <h1>Ta Kontakt</h1>
         <div>
             <svg xmlns="http://www.w3.org/2000/svg" className="ikon" width="15px" height="15px" viewBox="0 0 496 512"><path d="M336.5 160C322 70.7 287.8 8 248 8s-74 62.7-88.5 152h177zM152 256c0 22.2 1.2 43.5 3.3 64h185.3c2.1-20.5 3.3-41.8 3.3-64s-1.2-43.5-3.3-64H155.3c-2.1 20.5-3.3 41.8-3.3 64zm324.7-96c-28.6-67.9-86.5-120.4-158-141.6 24.4 33.8 41.2 84.7 50 141.6h108zM177.2 18.4C105.8 39.6 47.8 92.1 19.3 160h108c8.7-56.9 25.5-107.8 49.9-141.6zM487.4 192H372.7c2.1 21 3.3 42.5 3.3 64s-1.2 43-3.3 64h114.6c5.5-20.5 8.6-41.8 8.6-64s-3.1-43.5-8.5-64zM120 256c0-21.5 1.2-43 3.3-64H8.6C3.2 212.5 0 233.8 0 256s3.2 43.5 8.6 64h114.6c-2-21-3.2-42.5-3.2-64zm39.5 96c14.5 89.3 48.7 152 88.5 152s74-62.7 88.5-152h-177zm159.3 141.6c71.4-21.2 129.4-73.7 158-141.6h-108c-8.8 56.9-25.6 107.8-50 141.6zM19.3 352c28.6 67.9 86.5 120.4 158 141.6-24.4-33.8-41.2-84.7-50-141.6h-108z"></path></svg>
@@ -46,7 +46,7 @@ const ContactInfo = () => (
 );
 
 const Copyright = () => (
-    <div>
+    <div className = "copyright">
         <p>MN regnskap. Opphavsrett © {new Date().getFullYear()} M&N Regnskap. All rights reserved.</p>
     </div>
 );
