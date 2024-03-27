cd2c25b81c35e0973d231f50bffe69b31a9c321b
Johan Tryti <s362059@oslomet.no>
2024-03-27 16:04:06 +0100

    Footer og navbar.
    Navbar er care proof of consept.

--- mnregnskap.client/src/components/NavBar.jsx
+++ mnregnskap.client/src/components/NavBar.jsx
@@ -1,93 +1,35 @@
+//Inspirasjon fra https://www.w3schools.com/css/css_navbar.asp og https://www.w3schools.com/css/css_dropdowns.asp
 import { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
 import './NavBar.css';
 import { LanguageContext } from '../languages/LanguageContext';
-import en from '../languages/en.json'; // Importer engelsk sprokdata
-import no from '../languages/no.json'; // Importer norsk sprokdata
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import en from '../languages/en.json'; // Engelsk språkdata
+import no from '../languages/no.json'; // Norsk språkdata
 
 export default class NavBar extends Component {
     static contextType = LanguageContext; // Gjor LanguageContext tilgjengelig i denne komponenten
 
-    constructor(props) {
-        super(props);
-        this.toggleNavbar = this.toggleNavbar.bind(this);
-        this.toggleDropdown = this.toggleDropdown.bind(this); // Ny binding
-        this.state = {
-            collapsed: true,
-            dropdownOpen: false // Ny tilstandsvariabel
-        };
-    }
-
-    toggleDropdown() {
-        this.setState(prevState => ({
-            dropdownOpen: !prevState.dropdownOpen
-        }));
-    }
-
-    toggleNavbar() {
-        this.setState({
-            collapsed: !this.state.collapsed
-        });
-    }
-
-    handleMouseEnter = () => {
-        this.setState({ dropdownOpen: true });
-    };
-
-    handleMouseLeave = () => {
-        this.setState({ dropdownOpen: false });
-    };
-
-
-    //FUNKY BUG:
-
-    //                                    <DropdownItem onClick={() => this.context.setLanguage('norsk')}>Norsk</DropdownItem>
-    //                               <DropdownItem onClick={() => this.context.setLanguage('english')}>English</DropdownItem>
-
-    //Den koden fungerer, men når eg oversetter blir dropdown-UIen feil på engelsk?!?!??!
-
     render() {
         const { language } = this.context; // Hent ut language fra context
-        const labels = language === 'norsk' ? no : en; // Velg riktig språkobjekt
+        const labels = language === 'norsk' ? no : en; // Velg riktig sprokobjekt
         return (
-            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow navbar" light>
-                <NavbarBrand tag={Link} to="/hjem" className="navbar-logo-container">
-                    <div className="navbar-logo-wrapper">
-                        <img src="/mn-regnskaplogo.png" alt="M&N Regnskap Logo" className="mr-2 navbar-logo" />
+            <div className="topnav">
+                <div className="navbar-logo-wrapper">
+                    <img src="/mn-regnskaplogo.png" alt="M&N Regnskap Logo" className="mr-2 navbar-logo" />
+                </div>
+                <div className= "subtopnav"></div>
+                <Link to="/hjem" className="navTekst">{labels.home}</Link>
+                <Link to="/team" className="navTekst">{labels.about}</Link>
+                <Link to="/deadlines" className="navTekst">{labels.deadlines}</Link>
+                <Link to="/contact" className="navTekst">{labels.contact_us}</Link>
+                <div className="dropDown">
+                    <a className="navTekst">{labels.language}</a>
+                    <div className="dropDownContent">
+                        <button onClick={() => this.context.setLanguage('norsk')} className="text-dark nav-link">{labels.english}</button>
+                        <button onClick={() => this.context.setLanguage('english')} className="text-dark nav-link">{labels.norwegian}</button>
                     </div>
-                </NavbarBrand>
-                <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-                <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-                    <ul className="navbar-nav flex-grow">
-                        <NavItem>
-                            <NavLink tag={Link} className="text-dark nav-link" to="/hjem">{labels.home}</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink tag={Link} className="text-dark nav-link" to="/team">{labels.about}</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink tag={Link} className="text-dark nav-link" to="/link">{labels.deadlines}</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink tag={Link} className="text-dark nav-link" to="/contact">
-                                {labels.contact_us}
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
-                                <DropdownToggle caret className="text-dark nav-link">
-                                    {language === 'norsk' ? 'Norsk' : 'English'}
-                                </DropdownToggle>
-                                <DropdownMenu>
-                                    <DropdownItem onClick={() => this.context.setLanguage('norsk')}>{labels.norwegian}</DropdownItem>
-                                    <DropdownItem onClick={() => this.context.setLanguage('english')}>{labels.english}</DropdownItem>
-                                </DropdownMenu>
-                            </Dropdown>
-                        </NavItem>
-                    </ul>
-                </Collapse>
-            </Navbar>
+                </div>
+            </div>
         );
     }
 }
--- mnregnskap.client/src/pages/Hjem.jsx
+++ mnregnskap.client/src/pages/Hjem.jsx
@@ -1,9 +1,6 @@
 ﻿import { useContext, useEffect } from 'react';
 import { LanguageContext } from '../languages/LanguageContext'; 
-
 import './Hjem.css';
-
-
 import en from '../languages/en.json'; // Engelsk språkdata
 import no from '../languages/no.json'; // Norsk språkdata
 
