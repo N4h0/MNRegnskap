b5a4dbbfe2deee1033f2f3321b564405ffd3c5a6
AmnaDastgir <s364520@oslomet.no>
2024-04-02 19:55:39 +0200

    layout
    oppdateringer på pynt

--- mnregnskap.client/src/NotFound.jsx
+++ mnregnskap.client/src/NotFound.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
 import './NotFound.css';
 
-
-/*const SentenceWithLink = () => {
-  const handleLinkClick = () => {
-    // Handlingen som skal utføres når lenken klikkes, for eksempel å åpne en ny fane
-    window.open('https://localhost:5173/hjem', '_blank');
-  };
-  */
-
 const NotFound = () => {
   return (
     <div className="container">
       <div className="text-container">
-        <h2>Oisann... Vi fant ikke siden du lette etter!</h2>
-        <p>Sikker på at du har skrevet inn riktig URL? Prøv på nytt {/*<span onClick={handleLinkClick} style={{cursor: 'pointer', color: 'blue', textDecoration: 'underline'}} >herifra.</span>*/} herifra.</p>
+        <h1><span className="with-line" >4</span>04</h1>
+        <p>Oisann... Vi fant ikke siden du lette etter! Sikker på at du har skrevet inn riktig URL? Prøv på nytt herifra:</p>
           <div className="logo-container">
-            <img src="/public/logo1.png" alt="Hjem" className="logo1" />
-            <img src="/public/logo2.png" alt="Chabot" className="logo2" />
-            <img src="/public/logo3.png" alt="logo 3" className="logo3" />
-            <img src="/public/logo4.png" alt="logo 4" className="logo4" /> 
+            <a href="/hjem">
+              <img src="/logo1.png" alt="Hjem" className="logo1" />
+            </a>
+            <a href="/chatbot">
+              <img src="/logo2.png" alt="logo 2" className="logo2" />
+            </a>
+            <a href="/contact">
+              <img src="/logo3.png" alt="logo 3" className="logo3" />  
+            </a>
         </div>
       </div>
       <div className="image-container">
-        <img src="minion.png" alt="Minion" />
+        <img src="astronaut.png" alt="astronaut" className="astronaut-picture" />
       </div>
     </div>
     
